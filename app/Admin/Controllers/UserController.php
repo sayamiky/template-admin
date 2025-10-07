7b8876b85e5f2e929e20dec638b540493d7919ae
sayamiky <irmakrisnayanti31@gmail.com>
2025-10-07 15:32:09 +0800

    move usercontroller into admin

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
+namespace App\Admin\Controllers;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Admin\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
