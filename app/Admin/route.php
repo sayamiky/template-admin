a9a6eaeb1a985dd630a48a88b88baacec822399f
sayamiky <irmakrisnayanti31@gmail.com>
2025-10-07 14:50:51 +0800

    adjust

--- app/Admin/route.php
+++ app/Admin/route.php
@@ -3,7 +3,7 @@
 use App\Admin\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'role:admin'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
     Route::get('/roles', [RoleController::class, 'index']);
     Route::post('/roles', [RoleController::class, 'store']);
     Route::post('/users/{userId}/roles', [RoleController::class, 'assignRoles']);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,6 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        using: function () {
-        Route::middleware('admin')
-            ->prefix('admin')
-            ->group(__DIR__.'/../app/Admin/route.php');
-        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
