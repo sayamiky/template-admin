64beaacda5ad8fee7e3ce5ec10b1619640be11c6
sayamiky <irmakrisnayanti31@gmail.com>
2025-10-09 11:50:45 +0800

    .

--- resources/views/admin/permissions/edit.blade.php
+++ resources/views/admin/permissions/edit.blade.php
@@ -10,14 +10,13 @@
                     <h5 class="card-header">Edit Permission</h5>
                     <div class="card-body">
                         {{-- Form action akan kita implementasikan pada tahap selanjutnya --}}
-                        <form action="#" method="POST">
+                        <form action="{{ route('permissions.update', $permission->id) }}" method="POST">
                             @csrf
                             @method('PUT')
                             <div class="mb-3">
                                 <label for="name" class="form-label">Nama Permission</label>
                                 <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                    id="name" name="name"
-                                    value="{{ old('name', $permission->name) }}"
+                                    id="name" name="name" value="{{ old('name', $permission->name) }}"
                                     placeholder="Masukkan nama permission" required />
                                 @error('name')
                                     <div class="invalid-feedback">
@@ -27,8 +26,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="guard_name" class="form-label">Guard Name</label>
-                                <input type="text" class="form-control"
-                                    id="guard_name" name="guard_name"
+                                <input type="text" class="form-control" id="guard_name" name="guard_name"
                                     value="{{ $permission->guard_name }}" readonly />
                                 <div class="form-text">Guard name tidak dapat diubah.</div>
                             </div>
